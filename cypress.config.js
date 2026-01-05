88c73bfca92ec336c93c8b5adb5763470c616892
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-01-05 12:38:47 +0530

    feat: enhance CI workflow and reporting for Cypress tests with Allure integration

--- cypress.config.js
+++ cypress.config.js
@@ -6,29 +6,26 @@ const {
 const {
 	createEsbuildPlugin,
 } = require('@badeball/cypress-cucumber-preprocessor/esbuild')
-const {
-	configureAllureAdapterPlugins,
-} = require('@mmisty/cypress-allure-adapter/plugins')
 
 module.exports = defineConfig({
 	video: false,
 	defaultCommandTimeout: 5000,
 	pageLoadTimeout: 10000,
-	ignoreTestFiles: ['*.js', '*.md'],
+	reporter: 'mocha-allure-reporter',
+	reporterOptions: {
+		resultsDir: 'allure-results',
+		clean: true,
+	},
 	e2e: {
 		specPattern: 'cypress/e2e/**/*.feature',
+		excludeSpecPattern: ['*.js', '*.md'],
 		async setupNodeEvents(on, config) {
 			const bundler = createBundler({
 				plugins: [createEsbuildPlugin(config)],
 			})
 			on('file:preprocessor', bundler)
 			await addCucumberPreprocessorPlugin(on, config)
-			await configureAllureAdapterPlugins(on, config)
 			return config
-		},
-		env: {
-			allure: true,
-			allureResultsPath: 'allure-results',
 		},
 	},
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,6 +3,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-import '@mmisty/cypress-allure-adapter/support';
+// Import commands.js using CommonJS syntax:
+require('./commands')
